244d501a0ffe808bc0fbec8fa31249fe5e513199
wilsaj <wilson.andrew.j+github@gmail.com>
2014-08-11 12:50:11 -0500

    dance around ansible permissions + Windows filesystem incompatibility

--- bootstrap_ansible.sh
+++ bootstrap_ansible.sh
@@ -13,5 +13,12 @@ ssh-keygen -f /root/.ssh/id_rsa -P ""
 cat /root/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
 ssh-keyscan localhost > /root/.ssh/known_hosts
 
+
+# Ansible needs to have host file not be executable, so copy ansible hosts file
+# to /tmp so we can change permissions if VM host is Windows (permissions are
+# editable or even a thing that exists on Windows filesystems).
+cp /vagrant/vagrant_ansible_hosts /tmp/vagrant_ansible_hosts
+chmod -x /tmp/vagrant_ansible_hosts
+
 # run the ansible playbook to start making things osm
-ansible-playbook -i /vagrant/vagrant_ansible_hosts -u vagrant /vagrant/osm.yml
+ansible-playbook -i /tmp/vagrant_ansible_hosts -u vagrant /vagrant/osm.yml
