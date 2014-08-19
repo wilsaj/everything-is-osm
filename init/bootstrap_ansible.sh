2ae0084db9169c382049a5975643210ec1669b8f
wilsaj <wilson.andrew.j+github@gmail.com>
2014-08-19 11:04:20 -0500

    cleanup: move bootstrap script to init/ dir

--- init/bootstrap_ansible.sh
+++ init/bootstrap_ansible.sh
@@ -18,7 +18,7 @@ ssh-keyscan localhost > /root/.ssh/known_hosts
 # hosts file to /tmp and change permissions. This is necessary on Windows due to
 # the way filesystems are implemented: there isn't a way to change per-file
 # permissions on files in the shared /vagrant dir.
-cp /vagrant/vagrant_ansible_hosts /tmp/vagrant_ansible_hosts
+cp /vagrant/init/vagrant_ansible_hosts /tmp/vagrant_ansible_hosts
 chmod -x /tmp/vagrant_ansible_hosts
 
 # run the ansible playbook to start making things osm
